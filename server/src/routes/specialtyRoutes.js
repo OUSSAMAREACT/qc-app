da8e122271c00c0821f4fb5d4b52a6b188e3b325
Oussama Qarbach <61934836+OussamaQarbach@users.noreply.github.com>
2025-11-25 12:39:27 +0100

    fix: use requireAdmin middleware in specialtyRoutes

--- server/src/routes/specialtyRoutes.js
+++ server/src/routes/specialtyRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getSpecialties, createSpecialty, updateSpecialty, deleteSpecialty } from '../controllers/specialtyController.js';
-import { authenticateToken, isAdmin } from '../middleware/authMiddleware.js';
+import { authenticateToken, requireAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
@@ -8,8 +8,8 @@ const router = express.Router();
 router.get('/', getSpecialties);
 
 // Admin only: CRUD
-router.post('/', authenticateToken, isAdmin, createSpecialty);
-router.put('/:id', authenticateToken, isAdmin, updateSpecialty);
-router.delete('/:id', authenticateToken, isAdmin, deleteSpecialty);
+router.post('/', authenticateToken, requireAdmin, createSpecialty);
+router.put('/:id', authenticateToken, requireAdmin, updateSpecialty);
+router.delete('/:id', authenticateToken, requireAdmin, deleteSpecialty);
 
 export default router;
