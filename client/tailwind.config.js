7d5f8efba7fff76f4fb43fcd85f03855b0e740cf
Oussama Qarbach <61934836+OussamaQarbach@users.noreply.github.com>
2025-11-26 20:47:36 +0100

    fix: deployment crash (add start script and missing animations)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -29,6 +29,36 @@ export default {
           card: '#1e293b',
           border: '#334155',
         }
+      },
+      animation: {
+        blob: "blob 7s infinite",
+        'gradient-x': 'gradient-x 15s ease infinite',
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          },
+        },
       }
     },
   },
